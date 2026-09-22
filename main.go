98657ef8f868819ffed240344dc27a11fcf9bc75
raHI-79 <222550985+raHI-79@users.noreply.github.com>
2026-09-23 00:47:14 +0600

    Fix rate limiter

--- main.go
+++ main.go
@@ -3,60 +3,57 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"strings"
+	"os"
 	"sync"
 	"time"
 )
 type Client struct {
-	Count int
-	Time  time.Time
+	count int
+	time  time.Time
 }
 var clients = make(map[string]Client)
 var mu sync.Mutex
 const limit = 5
 func getIP(r *http.Request) string {
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return strings.TrimSpace(r.RemoteAddr)
-	}
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return ip
 }
 func rateLimit(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ip := getIP(r)
 		mu.Lock()
-		client, exists := clients[ip]
-		if !exists || time.Since(client.Time) >= time.Minute {
-			clients[ip] = Client{
-				Count: 1,
-				Time:  time.Now(),
-			}
+		client, ok := clients[ip]
+		if !ok || time.Since(client.time) >= time.Minute {
+			clients[ip] = Client{count: 1, time: time.Now()}
 			mu.Unlock()
 			next(w, r)
 			return
 		}
-		if client.Count >= limit {
+		if client.count >= limit {
 			mu.Unlock()
 			w.WriteHeader(http.StatusTooManyRequests)
-			fmt.Fprintln(w, "Too Many Requests! Try again later.")
+			fmt.Fprint(w, "Too Many Requests! Try again later.")
 			return
 		}
-		client.Count++
+		client.count++
 		clients[ip] = client
 		mu.Unlock()
 		next(w, r)
 	}
 }
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintln(w, "Request accepted!")
+	http.ServeFile(w, r, "index.html")
+}
+func request(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Request accepted!")
 }
 func main() {
-	http.HandleFunc("/", rateLimit(home))
-	fmt.Println("Server running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Server error:", err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+	http.HandleFunc("/", home)
+	http.HandleFunc("/request", rateLimit(request))
+    fmt.Println("Server running on http://localhost:" + port + "/")
+	http.ListenAndServe(":"+port, nil)
 }
